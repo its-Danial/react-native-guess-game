d9b4451cc97b94e9bc9fc54c99f9153e7e718aec
its-Danial <itsmalikdanial@gmail.com>
2022-08-11 22:22:23 +0800

    finished game over page layout

--- src/screens/GameOverScreen.tsx
+++ src/screens/GameOverScreen.tsx
@@ -1,13 +1,57 @@
 import { FC } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import Colors from "../../helpers/constants/Colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
 
 type GameOverScreenProps = {};
 
 const GameOverScreen: FC<GameOverScreenProps> = (props) => {
+  const startNewGamePressHandler = () => {};
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.container}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../../assets/images/success.png")} />
+      </View>
+      <Text style={styles.summaryText}>
+        You device needed <Text style={styles.highlightText}>X</Text> rounds to guess the number{" "}
+        <Text style={styles.highlightText}>Y</Text>{" "}
+      </Text>
+      <PrimaryButton onPress={startNewGamePressHandler}>Start New Game</PrimaryButton>
     </View>
   );
 };
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "FiraCode-Regular",
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  highlightText: {
+    fontFamily: "FiraCode-Bold",
+    color: Colors.primary600,
+  },
+});
